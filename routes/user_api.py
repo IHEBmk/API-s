fb210282f94719c9774cb1604b9a59483cd108a3
IHEBmk <145984657+IHEBmk@users.noreply.github.com>
2025-02-08 14:36:38 +0100

    Update user_api.py

--- routes/user_api.py
+++ routes/user_api.py
@@ -126,7 +126,7 @@ def get_current_user():
 @users_blueprint.route('/users/get_favourites/<int:user_id>', methods=['GET'])
 def get_Favourites(user_id):
     supabase = SupabaseClientSingleton()
-    response = supabase.from_('Favourites').select('reciepe_id').eq('user_id', user_id).all().execute()
+    response = supabase.from_('Favourites').select('reciepe_id').eq('user_id', user_id).execute().all()
     if response:
         return jsonify({"favourites": response.data}), 200
     else:
